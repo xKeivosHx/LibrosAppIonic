6e3ae162ba3f0cc644edd1262e5e93cb284eabd2
GatomanJuarez <juarez_devora_ja@hotmail.com>
2018-03-21 09:27:59 -0600

    Conexion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http';
+import { ToastController } from 'ionic-angular';
+import 'rxjs/Rx';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  libro: string;
+  resultado: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http:Http, private toastCtrl:ToastController) {
 
+  }
+
+  search(){
+    this.http.get('http://openlibrary.org/search.json?q=' + this.libro).map(res => res.json()).subscribe(data => {
+      this.resultado = data.docs;
+      console.log(this.resultado);
+     
+      console.log(this.resultado.length );
+      console.log("///////////////////////////////////////////////////");
+      for (var i = 0; i < this.resultado.length; i++){
+        console.log(this.resultado[i].title_suggest);
+      }
+      console.log("///////////////////////////////////////////////////");
+      
+      console.log("///////////////////////////////////////////////////");
+      for (var a = 0; a < this.resultado.length; a++){
+        console.log(this.resultado[a].first_publish_year);
+      }
+    });
+  }
+  
+  presentToast(message) {
+    let toast = this.toastCtrl.create({
+      message: ''+message ,
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
   }
 
 }
